547a274404968cc1f60e98a17e4359381375cb89
muruga21 <112178257+muruga21@users.noreply.github.com>
2022-11-26 19:55:24 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,58 @@
+# import the opencv library
+from PIL.Image import ImageTransformHandler
+import cv2
+import numpy as np
+import pytesseract
+
+
+pytesseract.pytesseract.tesseract_cmd="C:/Program Files/Tesseract-OCR/tesseract.exe"
+
+cascade= cv2.CascadeClassifier("C:/Users/Admin/Downloads/haarcascade_russian_plate_number.xml")
+
+
+import cv2
+  
+  
+# define a video capture object
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, img = vid.read()
+  
+    # Display the resulting frame
+    cv2.imshow('frame', img)
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    nplate=cascade.detectMultiScale(gray,1.1,4)
+    #crop portion
+    for (x,y,w,h) in nplate:
+        wT,hT,cT=img.shape
+        a,b=(int(0.02*wT),int(0.02*hT))
+        plate=img[y+a:y+h-a,x+b:x+w-b,:]
+        #make the img more darker to identify LPR
+        kernel=np.ones((1,1),np.uint8)
+        plate=cv2.dilate(plate,kernel,iterations=1)
+        plate=cv2.erode(plate,kernel,iterations=1)
+        plate_gray=cv2.cvtColor(plate,cv2.COLOR_BGR2GRAY)
+        (thresh,plate)=cv2.threshold(plate_gray,127,255,cv2.THRESH_BINARY)
+        #read the text on the plate
+        read=pytesseract.image_to_string(plate)
+        read=''.join(e for e in read if e.isalnum())
+        stat=read[0:2]
+        cv2.rectangle(img,(x,y),(x+w,y+h),(51,51,255),2)
+        cv2.rectangle(img,(x-1,y-40),(x+w+1,y),(51,51,255),-1)
+        cv2.putText(img,read,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.9,(255,255,255),2)
+
+        cv2.imshow('plate',plate)
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
